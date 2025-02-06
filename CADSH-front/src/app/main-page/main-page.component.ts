3e060cfcf9016378afc7c609bb85221b72a79737
zhanikplanet <zhanik.planet1@gmail.com>
2025-02-06 15:06:56 +0500

    accordion completed 50% and started card

--- CADSH-front/src/app/main-page/main-page.component.ts
+++ CADSH-front/src/app/main-page/main-page.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { JumbotronComponent } from "./jumbotron/jumbotron.component";
 import { AboutUsComponent } from "./about-us/about-us.component";
 import { ServicesComponent } from "./services/services.component";
-import { IndustriesComponent } from "./industries/industries.component";
+import { ProjectsComponent } from "./projects/projects.component";
 import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [JumbotronComponent, AboutUsComponent, ServicesComponent, IndustriesComponent, FooterComponent],
+  imports: [JumbotronComponent, AboutUsComponent, ServicesComponent, ProjectsComponent, FooterComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
---
+++ CADSH-front/src/app/main-page/projects/projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+@Component({
+  selector: 'app-projects',
+  imports: [CardModule],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+
+}
